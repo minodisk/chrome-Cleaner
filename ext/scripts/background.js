dd5d068534ff97b2902bfaf8346e9b697bcba609
minodisk <daisuke.mino@gmail.com>
2012-03-21 13:00:46 +0900

    Implement omnibox shortcut.

--- ext/scripts/background.js
+++ ext/scripts/background.js
@@ -1,58 +1,148 @@
 (function() {
-  var DEFAULT;
+  var BrowsingData, Omnibox, Storage,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
-  DEFAULT = {
-    period: 'everything',
-    history: true,
-    downloads: true,
-    cache: true,
-    cookies: false,
-    passwords: false,
-    formData: false
-  };
+  Storage = (function() {
 
-  window.storage = {
-    get: function(key) {
+    Storage.DEFAULT = {
+      period: 'everything',
+      history: true,
+      downloads: true,
+      cache: true,
+      cookies: false,
+      passwords: false,
+      formData: false
+    };
+
+    function Storage() {
+      var key, value, _ref;
+      _ref = Storage.DEFAULT;
+      for (key in _ref) {
+        value = _ref[key];
+        if (this.get(key) == null) this.set(key, value);
+      }
+    }
+
+    Storage.prototype.get = function(key) {
       var value;
       value = localStorage[key];
       if (value != null) value = JSON.parse(value);
       return value;
-    },
-    set: function(key, value) {
-      return localStorage[key] = JSON.stringify(value);
-    }
-  };
+    };
 
-  (function() {
-    var key, value, _results;
-    _results = [];
-    for (key in DEFAULT) {
-      value = DEFAULT[key];
-      if (storage.get(key) == null) {
-        _results.push(storage.set(key, value));
-      } else {
-        _results.push(void 0);
-      }
-    }
-    return _results;
+    Storage.prototype.set = function(key, value) {
+      return localStorage[key] = JSON.stringify(value);
+    };
+
+    Storage.prototype.getData = function() {
+      return {
+        period: this.get('period'),
+        dataToRemove: {
+          history: this.get('history'),
+          downloads: this.get('downloads'),
+          cache: this.get('cache'),
+          cookies: this.get('cookies'),
+          passwords: this.get('passwords'),
+          formData: this.get('formData')
+        }
+      };
+    };
+
+    return Storage;
+
   })();
 
-  chrome.browserAction.onClicked.addListener(function(tab) {
-    var data;
-    data = {
-      period: storage.get('period'),
-      dataToRemove: {
-        history: storage.get('history'),
-        downloads: storage.get('downloads'),
-        cache: storage.get('cache'),
-        cookies: storage.get('cookies'),
-        passwords: storage.get('passwords'),
-        formData: storage.get('formData')
-      }
+  BrowsingData = (function() {
+
+    function BrowsingData(_storage) {
+      var _this = this;
+      this._storage = _storage;
+      chrome.browserAction.onClicked.addListener(function() {
+        return _this.clear(_this._storage.getData());
+      });
+    }
+
+    BrowsingData.prototype.clear = function(data) {
+      if (data.period == null) data.period = this._storage.get('period');
+      return chrome.experimental.clear.browsingData(data.period, data.dataToRemove, function() {
+        return webkitNotifications.createHTMLNotification("notification.html#" + (JSON.stringify(data))).show();
+      });
     };
-    return chrome.experimental.clear.browsingData(data.period, data.dataToRemove, function() {
-      return webkitNotifications.createHTMLNotification("notification.html#" + (JSON.stringify(data))).show();
-    });
-  });
+
+    return BrowsingData;
+
+  })();
+
+  Omnibox = (function() {
+
+    Omnibox.FLAGS = (function() {
+      var FLAGS, flag, i, keyword;
+      FLAGS = {
+        h: 'history',
+        d: 'downloads',
+        c: 'cache',
+        k: 'cookies',
+        p: 'passwords',
+        f: 'formData'
+      };
+      for (flag in FLAGS) {
+        keyword = FLAGS[flag];
+        i = keyword.indexOf(flag);
+        FLAGS[flag] = {
+          keyword: keyword,
+          keys: [keyword.substr(0, i - 1), keyword.substr(i, 1), keyword.substr(i + 1)]
+        };
+      }
+      return FLAGS;
+    })();
+
+    function Omnibox(_browsingData) {
+      this._browsingData = _browsingData;
+      this._clear = __bind(this._clear, this);
+      this._setDefaultSuggestion('');
+      chrome.omnibox.onInputChanged.addListener(this._setDefaultSuggestion);
+      chrome.omnibox.onInputEntered.addListener(this._clear);
+    }
+
+    Omnibox.prototype._setDefaultSuggestion = function(text) {
+      var d, desc, flag, keys, _ref;
+      desc = [];
+      _ref = Omnibox.FLAGS;
+      for (flag in _ref) {
+        keys = _ref[flag].keys;
+        if (text.indexOf(flag) !== -1) {
+          d = "<url>" + keys[0] + "<match>" + keys[1] + "</match>" + keys[2] + "</url>";
+        } else {
+          d = "<dim>" + keys[0] + "</dim><match>" + keys[1] + "</match><dim>" + keys[2] + "</dim>";
+        }
+        desc.push(d);
+      }
+      return chrome.omnibox.setDefaultSuggestion({
+        description: desc.join(', ')
+      });
+    };
+
+    Omnibox.prototype._clear = function(text) {
+      var data, flag, keyword, _ref;
+      data = {
+        dataToRemove: {}
+      };
+      _ref = Omnibox.FLAGS;
+      for (flag in _ref) {
+        keyword = _ref[flag].keyword;
+        data.dataToRemove[keyword] = text.indexOf(flag) !== -1;
+      }
+      return this._browsingData.clear(data);
+    };
+
+    return Omnibox;
+
+  })();
+
+  this.storage = new Storage();
+
+  this.browsingData = new BrowsingData(this.storage);
+
+  new Omnibox(this.browsingData);
 
 }).call(this);
--- src/scripts/background.coffee
+++ src/scripts/background.coffee
@@ -1,35 +1,99 @@
-DEFAULT =
-  period   : 'everything'
-  history  : true
-  downloads: true
-  cache    : true
-  cookies  : false
-  passwords: false
-  formData : false
+class Storage
 
-window.storage =
+  @DEFAULT:
+    period   : 'everything'
+    history  : true
+    downloads: true
+    cache    : true
+    cookies  : false
+    passwords: false
+    formData : false
+
+  constructor: ->
+    for key, value of Storage.DEFAULT
+      unless @get(key)?
+        @set key, value
+
   get: (key)->
     value = localStorage[key]
     if value?
       value = JSON.parse value
     value
+
   set: (key, value)->
     localStorage[key] = JSON.stringify value
 
-do ->
-  for key, value of DEFAULT
-    unless storage.get(key)?
-      storage.set key, value
-
-chrome.browserAction.onClicked.addListener (tab)->
-  data =
-    period      : storage.get 'period'
+  getData: ->
+    {
+    period      : @get 'period'
     dataToRemove:
-      history  : storage.get 'history'
-      downloads: storage.get 'downloads'
-      cache    : storage.get 'cache'
-      cookies  : storage.get 'cookies'
-      passwords: storage.get 'passwords'
-      formData : storage.get 'formData'
-  chrome.experimental.clear.browsingData data.period, data.dataToRemove, ->
-    webkitNotifications.createHTMLNotification("notification.html##{JSON.stringify(data)}").show()
+      history  : @get 'history'
+      downloads: @get 'downloads'
+      cache    : @get 'cache'
+      cookies  : @get 'cookies'
+      passwords: @get 'passwords'
+      formData : @get 'formData'
+    }
+
+class BrowsingData
+
+  constructor: (@_storage)->
+    chrome.browserAction.onClicked.addListener =>
+      @clear @_storage.getData()
+
+  clear: (data)->
+    unless data.period?
+      data.period = @_storage.get 'period'
+    chrome.experimental.clear.browsingData data.period, data.dataToRemove, ->
+      webkitNotifications
+      .createHTMLNotification("notification.html##{JSON.stringify(data)}")
+      .show()
+
+class Omnibox
+
+  @FLAGS: do ->
+    FLAGS =
+    h: 'history'
+    d: 'downloads'
+    c: 'cache'
+    k: 'cookies'
+    p: 'passwords'
+    f: 'formData'
+    for flag, keyword of FLAGS
+      i = keyword.indexOf flag
+      FLAGS[flag] =
+        keyword: keyword
+        keys   : [
+          keyword.substr 0, i - 1
+          keyword.substr i, 1
+          keyword.substr i + 1
+        ]
+    FLAGS
+
+  constructor: (@_browsingData)->
+    @_setDefaultSuggestion ''
+    chrome.omnibox.onInputChanged.addListener @_setDefaultSuggestion
+    chrome.omnibox.onInputEntered.addListener @_clear
+
+  _setDefaultSuggestion: (text)->
+    desc = []
+    for flag, { keys } of Omnibox.FLAGS
+      if text.indexOf(flag) isnt -1
+        d = "<url>#{keys[0]}<match>#{keys[1]}</match>#{keys[2]}</url>"
+      else
+        d = "<dim>#{keys[0]}</dim><match>#{keys[1]}</match><dim>#{keys[2]}</dim>"
+      desc.push d
+    chrome.omnibox.setDefaultSuggestion {
+    description: desc.join ', '
+    }
+
+  _clear: (text)=>
+    data =
+    dataToRemove: {}
+    for flag, { keyword } of Omnibox.FLAGS
+      data.dataToRemove[keyword] = text.indexOf(flag) isnt -1
+    @_browsingData.clear data
+
+@storage = new Storage()
+@browsingData = new BrowsingData @storage
+new Omnibox @browsingData
